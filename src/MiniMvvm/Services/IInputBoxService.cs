41b140878d6776f96f2488c50114c55b914a19a7
Davi Mendes <davidmlsdg@gmail.com>
2022-01-24 19:06:52 -0300

    Simplify class InputBoxService

--- src/MiniMvvm/Services/IInputBoxService.cs
+++ src/MiniMvvm/Services/IInputBoxService.cs
@@ -1,69 +1,17 @@
-﻿using System.Windows.Media;
-
-namespace BasicMiniMvvm.Services
+﻿namespace BasicMiniMvvm.Services
 {
+    /// <summary>
+    /// IInputBoxService interface
+    /// </summary>
     public interface IInputBoxService
     {
         /// <summary>
-        /// Gets or sets the color of the box background.
-        /// </summary>
-        /// <value>
-        /// The color of the box background.
-        /// </value>
-        Brush BoxBackgroundColor { get; set; }
-        /// <summary>
-        /// Gets or sets the default text.
-        /// </summary>
-        /// <value>
-        /// The default text.
-        /// </value>
-        string DefaultText { get; set; }
-        /// <summary>
-        /// Gets or sets the font.
-        /// </summary>
-        /// <value>
-        /// The font.
-        /// </value>
-        FontFamily Font { get; set; }
-        /// <summary>
-        /// Gets or sets the size of the font.
-        /// </summary>
-        /// <value>
-        /// The size of the font.
-        /// </value>
-        int FontSize { get; set; }
-        /// <summary>
-        /// Gets the color of the input background.
-        /// </summary>
-        /// <value>
-        /// The color of the input background.
-        /// </value>
-        Brush InputBackgroundColor { get; }
-        /// <summary>
-        /// Gets or sets the ok button text.
-        /// </summary>
-        /// <value>
-        /// The ok button text.
-        /// </value>
-        string OkButtonText { get; set; }
-        /// <summary>
-        /// Gets or sets the prompt.
-        /// </summary>
-        /// <value>
-        /// The prompt.
-        /// </value>
-        string Prompt { get; set; }
-        /// <summary>
-        /// Gets or sets the title.
-        /// </summary>
-        /// <value>
-        /// The title.
-        /// </value>
-        string Title { get; set; }
-        /// <summary>
         /// Shows the dialog.
         /// </summary>
+        /// <param name="prompt">The prompt.</param>
+        /// <param name="title">The title.</param>
+        /// <param name="defaultText">The default text.</param>
         /// <returns></returns>
-        string ShowDialog();
+        string ShowDialog(string prompt, string title = "", string defaultText = "");
     }
 }
--- src/MiniMvvm/Services/InputBox.cs
+++ src/MiniMvvm/Services/InputBox.cs
@@ -10,164 +10,101 @@ using System.Windows.Media;
 namespace BasicMiniMvvm.Services
 {
     /// <summary>
-    /// InputBoxService represent the input box
+    /// InputBoxService class
     /// </summary>
-    /// <seealso cref="MiniMvvm.Services.IInputBoxService" />
+    /// <seealso cref="BasicMiniMvvm.Services.IInputBoxService" />
     public class InputBoxService : IInputBoxService
     {
-        Window window = new Window();
-        StackPanel stackPanel = new StackPanel();
-        bool clicked = false;
-        TextBox input = new TextBox();
-        Button ok = new Button();
-        bool inputreset = false;
+        private TextBox textBox;
+        private Window window;
 
         /// <summary>
-        /// Gets or sets the font.
-        /// </summary>
-        /// <value>
-        /// The font.
-        /// </value>
-        public FontFamily Font { get; set; } = new FontFamily("Tahoma");
-        /// <summary>
-        /// Gets or sets the size of the font.
-        /// </summary>
-        /// <value>
-        /// The size of the font.
-        /// </value>
-        public int FontSize { get; set; } = 30;
-        /// <summary>
-        /// Gets or sets the title.
-        /// </summary>
-        /// <value>
-        /// The title.
-        /// </value>
-        public string Title { get; set; } = "";
-        /// <summary>
-        /// Gets or sets the prompt.
-        /// </summary>
-        /// <value>
-        /// The prompt.
-        /// </value>
-        public string Prompt { get; set; } = "";
-        /// <summary>
-        /// Gets or sets the default text.
-        /// </summary>
-        /// <value>
-        /// The default text.
-        /// </value>
-        public string DefaultText { get; set; } = "";
-        /// <summary>
-        /// Gets or sets the ok button text.
-        /// </summary>
-        /// <value>
-        /// The ok button text.
-        /// </value>
-        public string OkButtonText { get; set; } = "OK";
-        /// <summary>
-        /// Gets or sets the color of the box background.
-        /// </summary>
-        /// <value>
-        /// The color of the box background.
-        /// </value>
-        public Brush BoxBackgroundColor { get; set; } = Brushes.GreenYellow;
-        /// <summary>
-        /// Gets the color of the input background.
-        /// </summary>
-        /// <value>
-        /// The color of the input background.
-        /// </value>
-        public Brush InputBackgroundColor { get; } = Brushes.Ivory;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="InputBoxService"/> class.
+        /// Shows the inputbox.
         /// </summary>
         /// <param name="prompt">The prompt.</param>
         /// <param name="title">The title.</param>
         /// <param name="defaultText">The default text.</param>
-        /// <param name="font">The font.</param>
-        /// <param name="fontsize">The fontsize.</param>
-        public InputBoxService(string prompt, string title = "", string defaultText = "", string font = "Tahoma", int fontsize = 30)
+        /// <returns>String</returns>
+        public string ShowDialog(string prompt, string title = "", string defaultText = "")
         {
             if (prompt == null)
                 prompt = "";
             if (title == null)
                 title = "";
-            if (string.IsNullOrWhiteSpace(font))
-                font = "Tahoma";
             if (defaultText == null)
                 defaultText = "";
 
-            Prompt = prompt;
-            Font = new FontFamily(font);
-            Title = title;
-            if (fontsize >= 1)
-                FontSize = fontsize;            
+            var window = CreateWindow(prompt, title, defaultText);
+            if (window.ShowDialog() == true)
+                return textBox.Text;
+
+            return defaultText;
         }
 
-        private void Windowdef()// window building - check only for window size
+        private Window CreateWindow(string prompt, string title, string defaultText)
         {
-            window.Height = 500;// Box Height
-            window.Width = 300;// Box Width
-            window.Background = BoxBackgroundColor;
-            window.Title = Title;
-            window.Content = stackPanel;
-            window.Closing += Box_Closing;
-            TextBlock content = new TextBlock();
-            content.TextWrapping = TextWrapping.Wrap;
-            content.Background = null;
-            content.HorizontalAlignment = HorizontalAlignment.Center;
-            content.Text = Prompt;
-            content.FontFamily = Font;
-            content.FontSize = FontSize;
-            stackPanel.Children.Add(content);
-
-            input.Background = InputBackgroundColor;
-            input.FontFamily = Font;
-            input.FontSize = FontSize;
-            input.HorizontalAlignment = HorizontalAlignment.Center;
-            input.Text = DefaultText;
-            input.MinWidth = 200;
-            input.MouseEnter += Input_MouseDown;
-            stackPanel.Children.Add(input);
-            ok.Width = 70;
-            ok.Height = 30;
-            ok.Click += Ok_Click;
-            ok.Content = OkButtonText;
-            ok.HorizontalAlignment = HorizontalAlignment.Center;
-            stackPanel.Children.Add(ok);
-
-        }
-
-        void Box_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            if (!clicked)
-                e.Cancel = true;
-        }
-
-        private void Input_MouseDown(object sender, MouseEventArgs e)
-        {
-            if ((sender as TextBox).Text == DefaultText && inputreset == false)
+            window = new Window();
+            window.Title = title;
+            window.MinWidth = 300;
+            window.SizeToContent = SizeToContent.WidthAndHeight;
+            StackPanel stackPanel = new StackPanel
             {
-                (sender as TextBox).Text = null;
-                inputreset = true;
-            }
+                Orientation = Orientation.Vertical,
+                Margin = new Thickness(3),
+            };
+            var textBlock = new TextBlock
+            {
+                Text = prompt,
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(3),
+            };
+            textBox = new TextBox
+            {
+                Text = defaultText,
+                Margin = new Thickness(3),
+            };
+            var stackPanelButtons = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                Margin = new Thickness(3)
+            };
+            var buttonOk = new Button
+            {
+                Content = "Ok",
+                Padding = new Thickness(30, 0, 30, 0),
+                Margin = new Thickness(3, 6, 3, 3),
+                HorizontalAlignment = HorizontalAlignment.Right,
+            };
+            buttonOk.Click += ButtonOk_Click;
+            var buttonCancel = new Button
+            {
+                Content = "Cancel",
+                Padding = new Thickness(30, 0, 30, 0),
+                Margin = new Thickness(3, 6, 3, 3),
+                HorizontalAlignment = HorizontalAlignment.Right,
+            };
+            buttonCancel.Click += ButtonCancel_Click;
+            stackPanelButtons.Children.Add(buttonOk);
+            stackPanelButtons.Children.Add(buttonCancel);
+            stackPanel.Children.Add(textBlock);
+            stackPanel.Children.Add(textBox);
+            stackPanel.Children.Add(stackPanelButtons);
+            window.Content = stackPanel;
+            return window;
         }
 
-        private void Ok_Click(object sender, RoutedEventArgs e)
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
         {
-            clicked = true;
+            window.DialogResult = true;
             window.Close();
-            clicked = false;
         }
 
-        public string ShowDialog()
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
-            Windowdef();
-            window.ShowDialog();
-            if (string.IsNullOrWhiteSpace(input.Text))
-                return DefaultText;
-            return input.Text;
+            textBox.Text = "";
+            window.DialogResult = false;
+            window.Close();
         }
     }
 }
